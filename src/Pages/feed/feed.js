950f0f26e7bf4385d6681c42d7dbdb6c647f41cb
Daiane Oltramari <daianeoltramari@gmail.com>
2022-04-13 18:28:57 -0300

    função login

---
+++ src/Pages/feed/feed.js
@@ -0,0 +1,14 @@
+export default function Timeline() {
+    const timeline = document.createElement('div');
+    timeline.innerHTML = `   
+        <div class="banner-menu">
+        <div class="banner">
+            <div class="title-container">
+                <h1 class="title">FEED</h1>
+                <h3 class="subtitle">Feed</h3>
+            </div>
+        </div>
+
+        `;
+    return timeline;
+}
